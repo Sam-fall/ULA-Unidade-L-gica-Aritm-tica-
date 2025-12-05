405de29b8542d6084ec1743beaf32092312f1abe
Sam-fall <samuel.meneses@unisantos.br>
2025-12-05 02:51:24 -0300

    atualizacao mux em logic

--- ULA/Modulo_logico/logic.v
+++ ULA/Modulo_logico/logic.v
@@ -1,3 +1,10 @@
+//Opcode
+//bit LSB = 1 inverte o valor, 0 fica igual
+//2bits MSM = 00 A and B 
+//            01 A or  B
+//            10 A xor B
+//            11   not A 
+
 module logic_mux_style (
     input [7:0] A,
     input [7:0] B,
@@ -5,18 +12,18 @@ module logic_mux_style (
     output [7:0] Out
 );
 
-    reg [7:0] selected_logic; // 'reg' porque recebe valor dentro do always
+    reg [7:0] selected_logic; 
 
     always @(*) begin
         case (Opcode[2:1]) 
-            2'b00: selected_logic = A & B; // AND
-            2'b01: selected_logic = A | B; // OR
-            2'b10: selected_logic = A ^ B; // XOR
-            2'b11: selected_logic = ~A;    // NOT
+            2'b00: selected_logic = A & B;
+            2'b01: selected_logic = A | B;
+            2'b10: selected_logic = A ^ B; 
+            2'b11: selected_logic = ~A;    
             default: selected_logic = 8'b0;
         endcase
     end
 
-    assign Out = selected_logic ^ {8{Opcode[0]}}; //Opcode 0 fica igual, Opcode 1 inverte
+    assign Out = selected_logic ^ {8{Opcode[0]}}; 
 
 endmodule
